a0d646f638d3b2d091db541f6c20d7f88afb91ec
= <=>
2020-03-03 08:59:09 +0200

    Pre-release

--- src/index.js
+++ src/index.js
@@ -3,7 +3,3 @@ import ReactDOM from 'react-dom';
 import App from './components/_App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-if (module.hot) {
-    module.hot.accept();
-}
